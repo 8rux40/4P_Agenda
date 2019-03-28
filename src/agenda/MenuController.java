62d78ec926676c3e5f300fbd55293015b61bfc79
Bruno Tardin <brunotardin20@gmail.com>
2019-03-27 21:20:24 -0300

    Criada View e esqueleto do controller

--- src/agenda/MenuController.java
+++ src/agenda/MenuController.java
@@ -2,7 +2,10 @@ package agenda;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
 
 /**
  * FXML Controller class
@@ -11,9 +14,46 @@ import javafx.fxml.Initializable;
  */
 public class MenuController implements Initializable {
 
+    @FXML
+    public MenuItem miInclusao;
+    @FXML
+    public MenuItem miEdicao;
+    @FXML
+    public MenuItem miExclusao;
+    @FXML
+    public MenuItem miListagem;
+    @FXML
+    public MenuItem miSair;
+    @FXML
+    public MenuItem miDesenvolvedor;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    public void onMiInclusaoAction(ActionEvent event) {
+    }
+
+    @FXML
+    public void onMiEdicaoAction(ActionEvent event) {
+    }
+
+    @FXML
+    public void onMiExclusaoAction(ActionEvent event) {
+    }
+
+    @FXML
+    public void onMiListagemAction(ActionEvent event) {
+    }
+
+    @FXML
+    public void onMiSairAction(ActionEvent event) {
+    }
+
+    @FXML
+    public void onMiDesenvolvedor(ActionEvent event) {
+    }
     
 }
